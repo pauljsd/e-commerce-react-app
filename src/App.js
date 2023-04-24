1a1b9e4f6a48db909c198dfdcb0503b94ee8d800
Atolagbe Paul <paul.atolagbe.ayobami@gmail.com>
2023-04-24 21:26:24 +0100

    created header componet with links to shop and contact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
+
 import HomePage from "./pages/homepage/Homepage";
 import ShopPage from "./pages/shoppage/ShopPage";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
