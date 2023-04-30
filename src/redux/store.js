0df73d457fd87f6d565dab19f4c329dba3703918
Atolagbe Paul <paul.atolagbe.ayobami@gmail.com>
2023-04-30 11:44:46 +0100

    added redux, react-redux, logger then implemented userReducer to header and app.js

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { legacy_createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import rootReducer from "./root-reducer";
+
+const middlewares = [logger];
+
+const store = legacy_createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
